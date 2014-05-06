c9f6229aba9827f9ef5448db6658dadf81d04ca2
rishabh23 <rishabh23@david-brearley.dreamhost.com>
2014-05-05 21:49:27 -0700

    base checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialshadeCom::Application.config.secret_token = '79994aed68a9efa3f691f56af4f9c3e744071618499f8ba6359c8704635073aa8d8c810c0718c2c6126a04cbeed7849aa67b3740cbc3e3e15e42e58cf86f6dba'
