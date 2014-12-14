dba44af8094e8d91615a6ecdd3fe6c74ceadcd33
rishabh23 <rishabh23@david-brearley.dreamhost.com>
2014-12-14 12:25:31 -0800

    server fixes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-OpenansCom::Application.config.session_store :cookie_store, key: '_openans.com_session'
+OpenansCom::Application.config.session_store :cookie_store, :key => '_openans.com_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
